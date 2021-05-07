59cc91b8603873be91851a2e1f262ff55f2c91f4
ADMIN <viettranaptx4869@gmail.com>
2021-05-07 11:49:51 +0700

    111111

---
+++ app/src/main/java/com/viet/exerciseui2/EmailActivity.kt
@@ -0,0 +1,12 @@
+package com.viet.exerciseui2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email)
+    }
+}
